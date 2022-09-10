4694c2be0b9ce24a85f729e5c7634519e0c5b2fa
“segadeyemi72” <segadeyemi72@gmail.com>
2022-09-10 08:17:55 -0700

    c program

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,21 +1,14 @@
- /*
-  *  * File: 101-quote.c
-  *   * Auth: Brennan D Baraban
-  *    */
-
+#include <stdio.h>
 #include <unistd.h>
 
 /**
- *  * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
- *   *                followed by a new line, to standard error.
- *    *
- *     * Return: Always 1.
- *      */
+* main - prints exactly "and that piece of art is useful"
+* - Dora Korpar, 2015-10-19",
+*   followed by a new line, to the standard error.
+* Return: 1 on success
+*/
 int main(void)
 {
-		write(2,
-					      "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
-					      	      59);
-
-			return (1);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return (1);
 }
