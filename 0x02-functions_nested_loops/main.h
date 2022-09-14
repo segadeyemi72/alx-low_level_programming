d3f0c868b9a3ca6e71a93bfa00048deadc2db202
“segadeyemi72” <segadeyemi72@gmail.com>
2022-09-14 05:15:48 -0700

    Write a program that prints _putchar,

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,19 +1,20 @@
-#ifndef HOLBERTON_H_INCLUDED
-#define HOLBERTON_H_INCLUDED
+#ifndef MAIN_H
+#define MAIN_H
 
-int _putchar(char);
+int _putchar(char c);
+void _print(int n);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
-int _abs(int);
-int print_last_digit(int);
+int _abs(int n);
+int print_last_digit(int n);
 void jack_bauer(void);
 void times_table(void);
-int add(int a, int b);
-void print_to_98(int n);
-void print_times_table(int n);
-void num(int r, int n);
+int add(int , int);
+void print_to_98(int);
+void print_times_table(int);
 
 #endif
+
