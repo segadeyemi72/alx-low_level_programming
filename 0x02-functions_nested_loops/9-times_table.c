e03c7570797b4fc04fc18710aa6514064f55b5b2
“segadeyemi72” <segadeyemi72@gmail.com>
2022-09-14 11:09:25 -0700

    Learn your times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -28,21 +28,20 @@ void times_table(void)
 				}
 				if (b < 9)
 				{
-					_putchar(',');
+ 				        _putchar(',');
 					_putchar(' ');
 				}
-				else
+			}
+			else
+			{
+				_putchar ((c / 10) + '0');
+				_putchar ((c % 10) + '0');
+				if (b < 9)
 				{
-					_putchar ((c / 10) + '0');
-					_putchar ((c % 10) + '0');
-					if (b < 9)
-					{
-						_putchar(',');
-						_putchar(' ');
-					}
+					_putchar(',');
+					_putchar(' ');
 				}
 			}
 			_putchar ('\n');
 		}
-	}
 }
