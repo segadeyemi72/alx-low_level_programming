3861bfdd7e0398c824a6794423dc14eaeacc6c90
“segadeyemi72” <segadeyemi72@gmail.com>
2022-09-16 02:23:17 -0700

    0-main.c

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -7,5 +7,8 @@ void positive_or_negative(int 1);
 int largest_number(int a, int b, int c);
 void print_remaining_days(int month, int day, int year);
 int convert_day(int month, int day);
+void print_times_table(int n);
+int convert_day(int, int);
+void print_remaining_days(int, int, int);
 
 #endif /* MAIN_H */
